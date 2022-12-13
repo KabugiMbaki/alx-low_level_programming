098ae5f5f2c27afc3a54d5c2c9c9bc064febed2f
Kabugi Mbaki <jameskabugi88@gmail.com>
2022-12-13 14:03:35 -0800

    Close

--- 0x00-hello_world/1-task.c
+++ 0x00-hello_world/1-task.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- *  main - Entry point
  * 
+ * main Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("This is a great start");
+	printf("James second trials");
 	return(0);
-}	
+}
